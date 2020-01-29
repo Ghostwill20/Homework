85c35e2882c7cb0efb9a855b2c1003990e9def6b
Ghostwill20 <carwilli21@gmail.com>
2020-01-29 15:15:23 -0500

    f

--- javascript.js
+++ javascript.js
@@ -3,8 +3,24 @@ var Xbox = 7;
 var x = (ps * xbox);
 
 console.log(x);
-
-var a = "computer";
+var a = "comput'trueer";
 var b = 81;
 var y = (a/b);
 console.log(y);
+
+var haveMoney;
+var fastFood;
+var homeMade;
+var types;
+var resturants;
+
+haveMoney = "true";
+fastFood = "false";
+homeMade = "no"; 
+type = "burger";
+
+if (haveMoney){
+    if(fastFood)
+    if(type === burger) console.log("5guys")
+    
+}
