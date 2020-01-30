1cfa87f3776a00bb35a745d56f1c5f80211c355a
Ghostwill20 <carwilli21@gmail.com>
2020-01-30 15:11:43 -0500

    homework

--- javascript.js
+++ javascript.js
@@ -1,26 +1,50 @@
 var ps  = "the best";
-var Xbox = 7;
+var xbox = 7;
 var x = (ps * xbox);
 
 console.log(x);
-var a = "comput'trueer";
+
+var a = "comput";
 var b = 81;
-var y = (a/b);
+var y = (a/81);
 console.log(y);
 
-var haveMoney;
-var fastFood;
-var homeMade;
-var types;
-var resturants;
-
-haveMoney = "true";
-fastFood = "false";
-homeMade = "no"; 
-type = "burger";
-
-if (haveMoney){
-    if(fastFood)
-    if(type === burger) console.log("5guys")
-    
+while(false);{
+  console.log("hello");
 }
+
+function arearoot (len, wid){
+var arearoot = len * wid;
+return arearoot;
+};
+var area = arearoot (5, 10);
+console.log(area);
+
+
+//conditional
+
+var bug = 2;
+
+console.log(bug + 5);
+
+console.log(bug * 7);
+
+//dividing 14 into 3 will leave you with 9 with a remainder of 2
+
+console.log(bug % 3);
+
+console.log(bug % 2);
+// i think the anwser will be either one or zero
+// the anwser was 0 i was half right 
+
+
+if (bug % 2 == 0) 
+{console.log ("i like games");}
+
+else {console.log("no");
+}
+
+
+
+
+
