e443023cc7462f2f5548bee34bae3e0ab3be912f
Ghostwill20 <carwilli21@gmail.com>
2020-02-07 20:16:36 -0500

    array

--- array.js
+++ array.js
@@ -1,50 +1,27 @@
 // array
 var games = ["ps4", "xbox", "wii", "gameboy"];
 
-for(i = 0; i < games.length; i++){
-console.log(games[i]);
+for (i = 0; i < games.length; i++) {
+  console.log(games[i]);
 }
 //grocery list array
 
-var grocery = ["steak", "eggs", "mac and cheese", "cheese", "ice cream", "ceral", "milk", "shrimp", "tissue", "popcorn"];
- for (i = 0; i < grocery.length; i++);{
- console.log(grocery[0]);
- }
- //displayed steak
-for (i = 0; i < grocery.length; i++);{
-  console.log(grocery[11]);
-  //fail
-}
-for (i = 0; i < grocery.length; i++);{
-  console.log(grocery[10]);
-}// fail
-for (i = 0; i < grocery.sort; i++);{
-  console.log(grocery[9])
-  // displayed popcorn
-}
-x = i;
-console.log(x + "")
-// fail
-for(i = 0; i < grocery.length; i++){
-  console.log(grocery[i]);
-  }
-  //pass displayed all 10
+var grocery = ["steak", "eggs", "mac and cheese", "cheese", "ice cream", "cereal", "milk", "shrimp", "tissue", "popcorn"];
 
-  var grocery = ["Steak", "Eggs", "Mac and Cheese", "Cheese", "Ice cream", "Ceral", "Milk", "Shrimp", "Tissue", "Popcorn"]
-  for(i = 0; i < grocery.length; i++){
+var total = [100, 50, 25, 30, 70, 100, 60, 30, 50, 60];
+
+function Add(){
+  var input = document.getElementById('userInput').value;
+  for (i = 0; i < grocery.length; i++) {
     console.log(grocery[i]);
-    }
-// this work to make them uppercase by hitting the shift key
+    for (let i = 0; i < grocery.reduce; i++)
+    console.log("we have more");
 
+}
 
-//inventory array
-
-var ps4Games = ["Madden", "Fifa" ,"Gta" ,"Cod" ,"Rdr2"];
-var Madden = [0];
-var Fifa = [1];
-var Gta = [2];
-var Cod = [3];
-var Rdr2 = [4];
+  
+     
+ 
 
 
 
@@ -52,3 +29,31 @@ var Rdr2 = [4];
 
 
 
+
+//end of section 
+for (i = 0; i < grocery.length; i++) {
+  console.log(grocery[i]);
+
+  for (i = 0; i < grocery.length; i++); {
+    console.log(grocery[0]);
+  }
+  //displayed steak
+  for (i = 0; i < grocery.length; i++); {
+    console.log(grocery[11]);
+    //fail
+  }
+  for (i = 0; i < grocery.length; i++); {
+    console.log(grocery[10]);
+  }// fail
+  for (i = 0; i < grocery.sort; i++); {
+    console.log(grocery[9])
+    // displayed popcorn
+  }
+  x = i;
+  console.log(x + "")
+  // fail
+  for (i = 0; i < grocery.length; i++) {
+    console.log(grocery[i]);
+  }
+}
+}
--- logfunction.js
+++ logfunction.js
@@ -1,4 +1,4 @@
-var ps  = "the best";
+var ps = "the best";
 var xbox = 7;
 var x = (ps * xbox);
 
@@ -6,21 +6,37 @@ console.log(x);
 
 var a = "comput";
 var b = 81;
-var y = (a/81);
+var y = (a / 81);
 console.log(y);
+// var input examples
+var dog = "50"
+var cat = "50"
+var final = (dog - cat);
+
+console.log(final);
+// it should return a value of 0
+// the result match up
+
+var tiger = "500"
+var lion = "250"
+var result = (tiger * lion);
+console.log(result);
+// my result im looking for idk
+// the answer is 125000
 
 /*while(false);{
   console.log("hello");
 }
 */
-function arearoot (len, wid){
-var arearoot = len * wid;
-return arearoot;
+function arearoot(len, wid) {
+  var arearoot = len * wid;
+  return arearoot;
 };
-var area = arearoot (5, 10);
+var area = arearoot(5, 10);
+var area = arearoot (25, 25);
+var area = arearoot (9, 7);
 console.log(area);
-
-
+ //function practice
 
 
 //conditional
@@ -40,108 +56,113 @@ console.log(bug % 2);
 // the anwser was 0 i was half right 
 
 
-if (bug % 2 == 0) 
-{console.log ("i like games");}
+if (bug % 2 == 0) { console.log("i like games"); }
 
-else {console.log("no");
+else {
+  console.log("no");
 }
 
 var ilovemyps4 = "true"
 
-if (ilovemyps4 || 9 < 100){
-console.log("my fav type");
+if (ilovemyps4 || 9 < 100) {
+  console.log("my fav type");
 }
-if (ilovemyps4 && 9 < 100)
-{console.log("i hate xbox");}
- 
+if (ilovemyps4 && 9 < 100) { console.log("i hate xbox"); }
 
- var ps4 = true
- if (ps4 > 5){
-   console.log(10)
 
- }
- else {(ps4 < 5) 
-   console.log(20)
- }
+var ps4 = true
+if (ps4 > 5) {
+  console.log(10)
+
+}
+else {
+  (ps4 < 5)
+  console.log(20)
+}
 
 
 
 //loop
 var a = 25
-for(var a = 25; a <= 125; a++){
-console.log(a);
+for (var a = 25; a <= 125; a++) {
+  console.log(a);
 }
 
 var word = 0
-while (word <= 50){
-  if(word % 2===0){
-  console.log(word);
+while (word <= 50) {
+  if (word % 2 === 0) {
+    console.log(word);
   }
   word++;
 }
 
-for (c = 0; c < 100; c++)  
-{ 
-if (c % 3 == 0) {
-console.log("fizz")
-}
-else if (c % 5 ==0)
-console.log("buzz")
-else (c % 15 ==0)
-console.log("fizzbuzz")
+for (c = 0; c < 100; c++) {
+  if (c % 3 == 0) {
+    console.log("fizz")
+  }
+  else if (c % 5 == 0)
+    console.log("buzz")
+  else (c % 15 == 0)
+  console.log("fizzbuzz")
 }
 
 var dog = 10
-while (dog <= 1000){
-if(dog % 10===0){
-  console.log(dog);
+while (dog <= 1000) {
+  if (dog % 10 === 0) {
+    console.log(dog);
   }
   dog++;
-if(dog * 10 ===0){
-  console.log(cat) 
-}
+  if (dog * 10 === 0) {
+    console.log(cat)
+  }
 
 
 }
+// i want to find numbers % 5 up to 500
+
+for (let i = 0; i <= 5; i++)
+  console.log(i)
+//  
+
 
 //exercise function
-function addition(mum1, mum2){
+function addition(mum1, mum2) {
   console.log(mum1 + mum2)
 }
 
-addition (50,125);
+addition(50, 125);
 
 
-function multiply(mum1, mum2){
-  console.log (mum1 * mum2); 
+function multiply(mum1, mum2) {
+  console.log(mum1 * mum2);
 }
 
 
-multiply (50, 125);
+multiply(50, 125);
 
 
 var a = 50
 var b = 125
-for(var a = 50; a <= 125; a++){
-console.log(a);
+for (var a = 50; a <= 125; a++) {
+  console.log(a);
 }
 
 //global scope
-function subtraction(mum1, mum2){
+function subtraction(mum1, mum2) {
   console.log(mum1 - mum2)
 }
 
-subtraction (25,100);
- 
- (function subtraction() {
+subtraction(25, 100);
+
+(function subtraction() {
   var x = 25;
   var y = 100;
   var answer = x - y;
-  console.log(x-y);
-  });
+  console.log(x - y);
+});
 //global function
- 
+
 //
-function randomCheck(){
-  var x = document.getElementById('userinput').value;z
-  }
+function randomCheck() {
+  var x = document.getElementById('userinput').value; z
+}
