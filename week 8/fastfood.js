a631577d7a22f5de61c86b3cd877a37fedbd942c
Ghostwill20 <carwilli21@gmail.com>
2020-03-18 15:36:15 -0400

    update2

---
+++ week 8/fastfood.js
@@ -0,0 +1,21 @@
+let items= [
+    'burger',
+    'fries',
+    'chicken',
+    'fish'
+];
+console.log(items);//successful
+
+let totals = [
+    50,
+    50,
+    50,
+    50
+];
+console.log(totals);//successful
+
+document.getElementById("menu").innerHTML = items
+document.getElementById("menu").innerHTML = totals
+
+
+
